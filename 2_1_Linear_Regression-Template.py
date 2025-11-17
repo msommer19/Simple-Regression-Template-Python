c06b2f9cb55e3fadb247876cecc990879539d771
Manuel <64355704+msommer19@users.noreply.github.com>
2025-11-17 17:14:50 +0100

    Implement feature encoding and backward elimination
    Added feature encoding and backward elimination for model optimization.

--- 2_1_Linear_Regression-Template.py
+++ 2_1_Linear_Regression-Template.py
@@ -9,6 +9,17 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+##############
+#IF NECESSARY: ELSE SKIP
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+##############
+
+
 #Splitting dataset into training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -27,3 +38,35 @@ print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),
 from sklearn.metrics import r2_score
 #to find similar functions check online information about scikitlearn: https://scikit-learn.org/stable/api/index.html
 r2_score(y_test, y_pred)
+
+#Backward-elimination for feature selection:
+'''Backward Elimination is irrelevant in Python, because the Scikit-Learn library
+automatically takes care of selecting the statistically significant
+features when training the model to make accurate predictions.
+However, if you do really want to learn how to 
+manually implement Backwards Elimination in Python and identify the most statistically significant features: '''
+
+
+# Building the optimal model using Backward Elimination
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_opt = X_opt.astype(np.float64)
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 1, 3, 4, 5]]
+X_opt = X_opt.astype(np.float64)
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 4, 5]]
+X_opt = X_opt.astype(np.float64)
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3, 5]]
+X_opt = X_opt.astype(np.float64)
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0, 3]]
+X_opt = X_opt.astype(np.float64)
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
